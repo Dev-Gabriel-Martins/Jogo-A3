4c1cb0d2b9a683fc14badc0c1733db0baceeb497
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2023-04-08 12:37:15 -0300

    adjusted import class Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,7 +3,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket; import javax.swing.JOptionPane;
+import java.net.Socket; 
+import javax.swing.JOptionPane;
+
 public class Cliente {
     public static void main(String args[]) throws IOException {
         System.out.println("Cliente iniciado...");
