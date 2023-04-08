65781c85bad72d8bf98669fa442a4a31dd5ad645
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2023-04-08 12:36:24 -0300

    Adjusted importa class Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,11 +1,17 @@
-import java.io.*; import java.net.Socket; import javax.swing.JOptionPane;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket; import javax.swing.JOptionPane;
 public class Cliente {
     public static void main(String args[]) throws IOException {
         System.out.println("Cliente iniciado...");
         Socket socket = new Socket("localhost",5000);
 
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedInputStream in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter         out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedInputStream  in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
         EscutaThread escuta = new EscutaThread(in);
         escuta.star();
         String entrada="";
